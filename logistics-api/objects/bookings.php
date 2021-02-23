473ca3b45e1f046ef917b8a2c5f2b5e9b24757fc
antlogist <antlogist@gmail.com>
2021-02-23 15:00:35 +0300

    Delivery status added to the table

--- logistics-api/objects/bookings.php
+++ logistics-api/objects/bookings.php
@@ -1,48 +1,77 @@
 <?php
 class Bookings {
 
-    // Table 'bookings' connection
-    private $conn;
-    private $table_name = "bookings";
+  // Table 'bookings' connection
+  private $conn;
+  private $table_name = "bookings";
+
+
+  // Object properties
+  public $id;
+  public $name;
+  public $phone;
+  public $email;
+  public $address;
+  public $date;
+  public $timeslot;
+  public $status;
+  public $start_date;
+  public $end_date;
+
+  // DB connection constructor
+  public function __construct($db){
+      $this->conn = $db;
+  }
+
+  // Read method
+  function read(){
+
+      // Select all of the records
+      $query = "SELECT
+                  id, name, phone, email, address, date, timeslot, status, order_id, order_token
+              FROM
+                  " . $this->table_name . "
+              WHERE
+                date BETWEEN '" . $this->start_date . "' AND '" . $this->end_date . "'
+              ORDER BY
+                  date DESC";
+
+      // Request preparing
+      $stmt = $this->conn->prepare($query);
+
+      // Request execution 
+      $stmt->execute();
+
+      return $stmt;
+  }
   
+  // Update method
+  function update() {
+    
+    // Request for updating the record
+    $query = "UPDATE
+            " . $this->table_name . "
+            SET
+                status = ?
+            WHERE
+                id = ?";
+    
+      // Request preparing
+      $stmt = $this->conn->prepare($query);
+    
+      // Sanitization 
+      $this->status=htmlspecialchars(strip_tags($this->status));
+      
+      // Parameters binding
+      $stmt->bindParam("1", $this->status);
+      $stmt->bindParam("2", $this->id);
+    
+        // Query execution 
+        if ($stmt->execute()) {
+            return true;
+        }
 
-    // Object properties
-    public $id;
-    public $name;
-    public $phone;
-    public $email;
-    public $address;
-    public $date;
-    public $timeslot;
-    public $status;
-    public $start_date;
-    public $end_date;
-
-    // DB connection constructor
-    public function __construct($db){
-        $this->conn = $db;
-    }
-
-    // Read method
-    function read(){
-
-        // Select all of the records
-        $query = "SELECT
-                    id, name, phone, email, address, date, timeslot, status, order_id, order_token
-                FROM
-                    " . $this->table_name . "
-                WHERE
-                  date BETWEEN '" . $this->start_date . "' AND '" . $this->end_date . "'
-                ORDER BY
-                    date DESC";
-
-        // Request preparing
-        $stmt = $this->conn->prepare($query);
-
-        // Request execution 
-        $stmt->execute();
-
-        return $stmt;
-    }
+        return false;
+  }
 }
 ?>
