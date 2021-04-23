a9ed97a8bc5aad20c8a7a56bc5c9c4e88aff0bf9
antlogist <antlogist@gmail.com>
2021-04-23 16:07:56 +0300

    Order serializing added

--- view-orders/src/store/modules/orders.js
+++ view-orders/src/store/modules/orders.js
@@ -1,20 +1,38 @@
 import ordersApi from "@/services/ordersApi";
 import mutations from "@/store/mutations";
 
+function serializeOrders(orders) {
+  return orders.reduce((acc, order) => {
+    let { id, created_at, total_price, payment_status, order_token } = order;
+    return [
+      ...acc,
+      {
+        id: id,
+        createdAt: created_at,
+        totalPrice: total_price,
+        paymentStatus: payment_status,
+        orderToken: order_token,
+        deliveryDate: "",
+        deliveryTimeslot: "",
+        deliveryStatus: ""
+      }
+    ];
+  }, []);
+}
+
 const { ORDERS } = mutations;
 
 const ordersStore = {
   namespaced: true,
   state: {
-    orders: [],
-    delivery: []
+    orders: []
   },
   getters: {
     orders: ({ orders }) => orders
   },
   mutations: {
     [ORDERS](state, orders) {
-      state.orders = orders;
+      state.orders = orders.length > 0 ? serializeOrders(orders) : orders;
     }
   },
   actions: {
@@ -24,13 +42,20 @@ const ordersStore = {
     async fetchDelivery({ state }, orders) {
       state.delivery = [];
       for (const order of orders) {
-        if (order.order_token) {
+        if (order.orderToken) {
           try {
             const response = await ordersApi.fetchDelivery(
-              order.created_at.substring(0, 10),
-              order.order_token
+              order.createdAt.substring(0, 10),
+              order.orderToken
             );
-            state.delivery.push(response);
+            // Add delivery info to the object
+            for (var i = 0; i < state.orders.length; i++) {
+              if (response.orderToken === state.orders[i].orderToken) {
+                state.orders[i].deliveryDate = response.date;
+                state.orders[i].deliveryTimeslot = response.timeslot;
+                break;
+              }
+            }
           } catch (err) {
             console.log(err);
           }
