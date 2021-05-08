f1729ce9bc0c6a1ed41149109ee3b128b262711b
antlogist <antlogist@gmail.com>
2021-05-08 23:30:23 +0300

    Timeslots dialogs added

--- logistics-admin/src/store/modules/defaultTimeslots.js
+++ logistics-admin/src/store/modules/defaultTimeslots.js
@@ -4,20 +4,24 @@ const defaultTimeslotsStore = {
     timeslots: {
       0: [
         {
+          id: "1",
           start_at: "07:00:00",
           end_at: "07:30:00"
         },
         {
+          id: "1",
           start_at: "07:30:00",
           end_at: "08:00:00"
         }
       ],
       1: [
         {
+          id: "1",
           start_at: "07:00:00",
           end_at: "07:30:00"
         },
         {
+          id: "1",
           start_at: "07:30:00",
           end_at: "08:00:00"
         }
@@ -25,20 +29,24 @@ const defaultTimeslotsStore = {
       2: [],
       3: [
         {
+          id: "1",
           start_at: "07:00:00",
           end_at: "07:30:00"
         },
         {
+          id: "1",
           start_at: "07:30:00",
           end_at: "08:00:00"
         }
       ],
       4: [
         {
+          id: "1",
           start_at: "07:00:00",
           end_at: "07:30:00"
         },
         {
+          id: "1",
           start_at: "07:30:00",
           end_at: "08:00:00"
         }
@@ -50,6 +58,17 @@ const defaultTimeslotsStore = {
   },
   getters: {
     timeslots: ({ timeslots }) => timeslots
+  },
+  actions: {
+    setCurrentWeekday({ state }, index) {
+      state.currentWeekday = index;
+    },
+    async fetchTimeslots({ state }) {
+      console.log(state);
+    },
+    async addDefaultTimeslot({ state }) {
+      console.log(state);
+    }
   }
 };
 
