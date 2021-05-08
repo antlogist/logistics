0e55b1c400cd066593f2c776f02b2c5e556747ab
antlogist <antlogist@gmail.com>
2021-05-08 12:06:12 +0300

    Timeslots component created

--- logistics-admin/src/router/index.js
+++ logistics-admin/src/router/index.js
@@ -4,6 +4,7 @@ import Calendar from "@/components/calendar/Calendar";
 import Bookings from "@/components/bookings/Bookings";
 import Report from "@/components/report/Report";
 import Map from "@/components/map/Map";
+import Timeslots from "@/components/timeslots/Timeslots";
 
 Vue.use(Router);
 
@@ -35,6 +36,11 @@ export function createRouter() {
         path: "/map",
         name: "Map",
         component: Map
+      },
+      {
+        path: "/timeslots",
+        name: "Timeslots",
+        component: Timeslots
       }
     ]
   });
